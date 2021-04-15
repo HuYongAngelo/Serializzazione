4e9c6e5f064c4740240ebe58be857c9e850c512a
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2021-04-15 14:10:35 +0200

    Float written

---
+++ Serializzazione/src/serializzazione/Studente.java
@@ -0,0 +1,13 @@
+package serializzazione;
+
+public class Studente {
+    private String nome;
+    private String cognome;
+    private Integer numeroRegistro;
+
+    public Studente(String nome, String cognome, Integer numeroRegistro) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeroRegistro = numeroRegistro;
+    }
+}
